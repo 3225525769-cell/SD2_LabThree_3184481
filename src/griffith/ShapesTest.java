8e7b14532cf3795ea5a3f78155299797af83f8a7
3225525769-cell <3225525769@qq.com>
2026-03-07 14:36:04 +0000

    git add ShapesTest.java git commit -m "test: add unit tests for testRhombus" git push

--- src/griffith/ShapesTest.java
+++ src/griffith/ShapesTest.java
@@ -2,14 +2,14 @@ package griffith;
 
 import static org.junit.jupiter.api.Assertions.*;
 
- public class shapesTest {
+ public class ShapesTest {
     private static final double EPSILON = 0.1;
 
     @org.junit.jupiter.api.Test
    public void testCircle() {
         Circle circle = new Circle("circle",3.5);
-        assertEquals(38.4845,circle.area(),EPSILON);
-        assertEquals(21.9911,circle.perimeter(),EPSILON);
+        assertEquals(38.48451,circle.area(),EPSILON);
+        assertEquals(21.99115,circle.perimeter(),EPSILON);
         assertTrue(circle.toString().contains("Radius:3.5"));
 
 
@@ -18,6 +18,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
     @org.junit.jupiter.api.Test
     void testRhombus() {
+        Rhombus rhombus = new Rhombus("Rhombus",5.0,60.0);
+        assertEquals(21.6506,rhombus.area(),EPSILON);
+        assertEquals(20.0,rhombus.perimeter(),EPSILON);
+        assertTrue(rhombus.toString().contains("Side:5.0")&&rhombus.toString().contains("Angle:60.0"));
+
+
     }
 
     @org.junit.jupiter.api.Test
